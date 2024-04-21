7d75a9cd145be3dce42b4e163b42e6df2ea1bb02
Andriikryl <andreyk557@gmail.com>
2024-04-21 17:36:40 +0300

    add data info

---
+++ src/app/data/info.ts
@@ -0,0 +1,305 @@
+export const deadCell = [
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: true,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+  ];
+  export const threeAliveNeighbors = [
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: true,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+  ];
+  export const aliveCell = [
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+  ];
+  export const oneAliveCell = [
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: true,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+  ];
+ export const fourAliveCell = [
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: true,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+  ];
+ export const overCrowding = [
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: true,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+  ];
+ export const threeAlive = [
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+  ];
+ export const threeAliveY = [
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+    {
+      cellType: "alive",
+      activeCell: false,
+    },
+    {
+      cellType: "dead",
+      activeCell: false,
+    },
+  ];
+  
